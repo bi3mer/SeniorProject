25a590d7dc5a53f65550ff3a6b708e54486cfc15
sap344 <patelshreya94@gmail.com>
2017-02-20 14:22:55 -0500

    Mac build script addition (#460)
    * inital macOSx build method
    
    * mac build script
    
    * copied files over to mac data path
    
    * fixes paused button and text in master scene and loads correct scene from menu
    
    * taking the pause menu issue #450 changes out of this PR
    
    * fixed based on gabby's comment

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -21,23 +21,65 @@ public class BuildScript : MonoBehaviour
         // Build player
         BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, Path.Combine(buildPath, executable + ".exe"), BuildTarget.StandaloneWindows, BuildOptions.None);
 
-        BuildScript.CopyYAMLFiles(buildPath);
+        BuildScript.CopyYAMLFilesWindows(buildPath);
     }
 
+	/// <summary>
+	/// Builds for macOSx including post process steps.
+	/// </summary>
+	[MenuItem("Build/MacOSx Build With Packaged YAML files")]
+	public static void BuildGameMacOSx()
+	{
+		// Get filename
+		string buildPath = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
+
+		// Build player
+		BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, Path.Combine(buildPath, executable + ".app"), BuildTarget.StandaloneOSXIntel64, BuildOptions.None);
+	
+		BuildScript.CopyYAMLFilesMac(buildPath);
+	}
+
     /// <summary>
-    /// Post process step for copying YAML files for build directory
+    /// Post process step for copying YAML files for windows build directory
     /// </summary>
     /// <param name="buildPath">The build path.</param>
-    public static void CopyYAMLFiles (string buildPath)
+    public static void CopyYAMLFilesWindows (string buildPath)
     {
-        // Copy all yaml files to the build directory
-        DirectoryInfo dir = new DirectoryInfo(yamlDirectory);
-        FileInfo[] info = dir.GetFiles("*.yml", SearchOption.TopDirectoryOnly);
-        foreach (FileInfo file in info)
-        {
-            string basePath = Path.Combine(buildPath, executable + "_Data");
-            string path = Path.Combine(basePath, file.Name);
-            FileUtil.CopyFileOrDirectory(file.FullName, path);
-        }
+		// Copy all yaml files to the build directory
+		DirectoryInfo dir = new DirectoryInfo(yamlDirectory);
+		FileInfo[] info = dir.GetFiles("*.yml", SearchOption.TopDirectoryOnly);
+
+		// create directory for yaml files in the buildpath location
+		string basePath = Path.Combine(buildPath, executable + "_Data");
+		System.IO.Directory.CreateDirectory (basePath);
+
+		// copy each yaml file to new location
+		foreach (FileInfo file in info)
+		{
+			string path = Path.Combine(basePath, file.Name);
+			FileUtil.CopyFileOrDirectory(file.FullName, path);
+		}
     }
+
+	/// <summary>
+	/// Post process step for copying YAML files for mac build directory
+	/// </summary>
+	/// <param name="buildPath">The build path.</param>
+	public static void CopyYAMLFilesMac (string buildPath)
+	{
+		// Copy all yaml files to the build directory
+		DirectoryInfo dir = new DirectoryInfo(yamlDirectory);
+		FileInfo[] info = dir.GetFiles("*.yml", SearchOption.TopDirectoryOnly);
+
+		// create directory for yaml files in the buildpath location
+		string basePath = Path.Combine(buildPath, executable + ".app/Contents");
+		System.IO.Directory.CreateDirectory (basePath);
+
+		// copy each yaml file to new location
+		foreach (FileInfo file in info)
+		{
+			string path = Path.Combine(basePath, file.Name);
+			FileUtil.CopyFileOrDirectory(file.FullName, path);
+		}
+	}
 }
