9843860dffa970500ab632a895fa367febb424fa
Gabby Getz <ggetz881@gmail.com>
2017-02-21 20:55:51 -0500

    Updated build options to include development builds (#493)

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -13,31 +13,69 @@ public class BuildScript : MonoBehaviour
     /// Build for windows including post process steps.
     /// </summary>
     [MenuItem("Build/Windows Build With Packaged YAML files")]
-    public static void BuildGame ()
+    public static void BuildGameWindows ()
     {
-        // Get filename
-        string buildPath = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
-
-        // Build player
-        BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, Path.Combine(buildPath, executable + ".exe"), BuildTarget.StandaloneWindows, BuildOptions.None);
-
-        BuildScript.CopyYAMLFilesWindows(buildPath);
+        buildGame(BuildTarget.StandaloneWindows, BuildOptions.None);
     }
 
-	/// <summary>
-	/// Builds for macOSx including post process steps.
-	/// </summary>
-	[MenuItem("Build/MacOSx Build With Packaged YAML files")]
-	public static void BuildGameMacOSx()
-	{
-		// Get filename
-		string buildPath = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
+    /// <summary>
+    /// Development build for windows including post process steps.
+    /// </summary>
+    [MenuItem("Build/Windows Development Build With Packaged YAML files")]
+    public static void BuildGameWindowsDevelopment()
+    {
+        buildGame(BuildTarget.StandaloneWindows, BuildOptions.Development);
+    }
 
-		// Build player
-		BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, Path.Combine(buildPath, executable + ".app"), BuildTarget.StandaloneOSXIntel64, BuildOptions.None);
-	
-		BuildScript.CopyYAMLFilesMac(buildPath);
-	}
+    /// <summary>
+    /// Builds for macOSx including post process steps.
+    /// </summary>
+    [MenuItem("Build/MacOSx Build With Packaged YAML files")]
+    public static void BuildGameMacOSx()
+    {
+        buildGame(BuildTarget.StandaloneOSXIntel64, BuildOptions.None);
+    }
+
+    /// <summary>
+    /// Development builds for macOSx including post process steps.
+    /// </summary>
+    [MenuItem("Build/MacOSx Development Build With Packaged YAML files")]
+    public static void BuildGameMacOSxDevelopment()
+    {
+        buildGame(BuildTarget.StandaloneOSXIntel64, BuildOptions.Development);
+    }
+
+    /// <summary>
+    /// Builds the game with the specified paramters.
+    /// </summary>
+    /// <param name="target">Target build platform.</param>
+    /// <param name="options">Build options</param>
+    private static void buildGame(BuildTarget target, BuildOptions options)
+    {
+        // Set up settings for each platform
+        string buildPath = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
+        string executableName = executable;
+        if (target == BuildTarget.StandaloneWindows)
+        {
+            executableName += ".exe";
+        }
+        else
+        {
+            executableName += ".app";
+        }
+
+        // Build player
+        BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, Path.Combine(buildPath, executableName), target, options);
+
+        if (target == BuildTarget.StandaloneWindows)
+        {
+            BuildScript.CopyYAMLFilesWindows(buildPath);
+        }
+        else
+        {
+            BuildScript.CopyYAMLFilesMac(buildPath);
+        }
+    } 
 
     /// <summary>
     /// Post process step for copying YAML files for windows build directory
