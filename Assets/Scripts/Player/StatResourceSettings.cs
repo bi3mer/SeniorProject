f6a4db007f7166c114b3ecad94e598f7f6cd5f03
Shreya Patel <patelshreya94@gmail.com>
2017-05-08 04:21:33 -0400

    editing stat resource settings

--- Assets/Scripts/Player/StatResourceSettings.cs
+++ Assets/Scripts/Player/StatResourceSettings.cs
@@ -1,16 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class StatResourceSettings : MonoBehaviour {
+[System.Serializable]
+public class StatResourceSettings
+{
+	public KeyCode Forward;
+	public KeyCode Back;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
