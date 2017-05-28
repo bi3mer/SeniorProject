b7b5046f200cc3d9ae6cee589004782c2d37d0c2
Gabby Getz <ggetz881@gmail.com>
2017-05-28 18:57:51 -0400

    City tests (#874)
    * Begining of unit tests
    
    * More city tests
    
    * Fixes for Laura

---
+++ Assets/Editor/EditorUnitTests/CityTests/GenerationUtilityTets.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+public class GenerationUtilityTests
+{
+	[Test]
+	public void ConvertsToAlignedVector3 ()
+	{
+		Vector3 result = GenerationUtility.ToAlignedVector3 (Vector2.right);
+
+		Assert.AreEqual(result.x, 1f);
+		Assert.AreEqual(result.y, 0f);
+		Assert.AreEqual(result.z, 0f);
+	}
+
+	[Test]
+	public void ConvertsToAlignedVector2 ()
+	{
+		Vector2 result = GenerationUtility.ToAlignedVector2 (Vector3.up);
+
+		Assert.AreEqual(result.x, 0f);
+		Assert.AreEqual(result.y, 0f);
+	}
+
+	[Test]
+	public void AddsNonDuplicateVertex ()
+	{
+		List<Vector2> verts = new List<Vector2>();
+
+		verts = GenerationUtility.AddNonDuplicateVertex(verts, Vector2.up);
+
+		Assert.AreEqual(verts.Count, 1);
+
+		verts = GenerationUtility.AddNonDuplicateVertex(verts, Vector2.up);
+
+		Assert.AreEqual(verts.Count, 1);
+	}
+
+	[Test]
+	public void ChecksPointInPolygon ()
+	{
+		Vector2[] polygon = new Vector2[] { Vector2.up, Vector2.right, Vector2.down, Vector2.left };
+
+		Assert.IsFalse(GenerationUtility.IsPointInPolygon(Vector2.up * 2, ref polygon));
+		Assert.IsTrue(GenerationUtility.IsPointInPolygon(Vector2.zero, ref polygon));
+	}
+
+	[Test]
+	public void GetsMidpoint ()
+	{
+		Vector2[] verts = new Vector2[] { Vector2.up, Vector2.right, Vector2.down, Vector2.left };
+
+		Vector2 result = GenerationUtility.GetMidpoint(new List<Vector2>(verts));
+
+		Assert.AreEqual(result, Vector2.zero);
+	}
+
+	[Test]
+	public void SortsClockwise ()
+	{
+		Vector2[] verts = new Vector2[] { Vector2.down, Vector2.up, Vector2.left, Vector2.right };
+
+		List<Vector2> result = GenerationUtility.SortVerticies(new List<Vector2>(verts), Vector2.zero);
+
+		Assert.AreEqual(result[0].x, 0.0);
+		Assert.AreEqual(result[0].y, -1.0);
+		Assert.AreEqual(result[1].x, 1.0);
+		Assert.AreEqual(result[1].y, 0.0);
+		Assert.AreEqual(result[2].x, 0.0);
+		Assert.AreEqual(result[2].y, 1.0);
+		Assert.AreEqual(result[3].x, -1.0);
+		Assert.AreEqual(result[3].y, 0.0);
+	}
+
+	[Test]
+	public void GetsMostCommonVertex ()
+	{
+		District d = new District (Vector3.zero, new Vector3[] { Vector3.up, Vector3.zero, Vector3.right }, new DistrictConfiguration());
+		District e = new District (Vector3.zero, new Vector3[] { Vector3.left, Vector3.right, Vector3.back }, new DistrictConfiguration());
+
+		Assert.AreEqual(Vector3.right, GenerationUtility.GetMostCommonVertex( new District[] { d, e } ));
+	}
+}
---
+++ Assets/Editor/EditorUnitTests/CityTests/TemplateBuildingTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+
+public class TemplateBuildingTest 
+{
+	[Test]
+	public void ConstructsTemplateBuilding ()
+	{
+		GameObject gameObject = new UnityEngine.GameObject();
+		Transform transform = gameObject.transform;
+
+		TemplateBuilding building = new TemplateBuilding(transform, Vector3.left, gameObject);
+
+		Assert.AreEqual(building.Parent, transform);
+		Assert.AreEqual(building.Position, Vector3.left);
+		Assert.AreEqual(building.Template, gameObject);
+		Assert.AreEqual(building.IsLoaded, false);
+		Assert.NotNull(building.Attachments);
+		Assert.IsNull(building.Instance);
+	}
+
+	[Test]
+	public void LoadsTemplateBuilding ()
+	{
+		GameObject gameObject = new GameObject();
+		Transform transform = gameObject.transform;
+
+		TemplateBuilding building = new TemplateBuilding(transform, Vector3.left, gameObject);
+
+		building.Load();
+
+		Assert.AreEqual(building.IsLoaded, true);
+		Assert.IsNotNull(building.Instance);
+		Assert.AreEqual(building.Instance.transform.position, Vector3.left);
+		Assert.AreEqual(building.Instance.transform.parent, transform);
+	}
+
+	[Test]
+	public void CalculatesBounds ()
+	{
+		GameObject gameObject = new GameObject();
+		Transform transform = gameObject.transform;
+
+		TemplateBuilding building = new TemplateBuilding(transform, Vector3.left, gameObject);
+
+		building.Load();
+
+		Assert.AreNotEqual(building.BoundingBox, new Bounds());
+	}
+}
