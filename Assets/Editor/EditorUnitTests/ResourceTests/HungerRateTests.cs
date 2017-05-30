f409bb4dcc8a0ff48a4b37bb0f05df7b70395576
sap344 <patelshreya94@gmail.com>
2017-05-29 23:49:52 -0400

    Resource tests (#885)
    * health tests
    
    * starting warmth and hunger tests
    
    * adding ignore tag
    
    * after invoke method fix

---
+++ Assets/Editor/EditorUnitTests/ResourceTests/HungerRateTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using System;
+
+[TestFixture]
+public class HungerRateTests
+{
+	private Player createTestPlayer()
+	{
+		PlayerInventory mockPlayerInventory = new PlayerInventory("player", 20);
+		PlayerController controller = new GameObject().AddComponent<PlayerController>();
+		controller.PlayerStatManager = new PlayerStatManager ();
+
+		Game.Instance.PlayerInstance = new Player(mockPlayerInventory);
+		Game.Instance.PlayerInstance.Controller = controller;
+
+		return Game.Instance.PlayerInstance;
+	}
+
+	[Test]
+	public void PlayerHungerShouldStartAtMaxHungerTest()
+	{
+		// Arrange
+		Player mockPlayer = createTestPlayer();
+
+		// Assert
+		Assert.AreEqual (mockPlayer.MaxHunger, mockPlayer.Hunger);
+	}
+
+	[Test]
+	public void PlayerHungerUseFoodEnergyBasedOnAmountTest()
+	{
+		// Arrange
+		Player mockPlayer = createTestPlayer();
+
+		// Act
+		mockPlayer.Controller.PlayerStatManager.HungerRate.UseFoodEnergy(-1);
+
+		// Assert
+		Assert.AreEqual (99, mockPlayer.Hunger);
+	}
+}
